b52c7a0e47deb09268d824a502fd9f1303e33a3b
Анастасия <nastya937kozlova@yandex.ru>
2023-05-24 23:00:49 +0400

    fix: code style in main

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-from db.database import DataBase
+"""File that is responsible for open file """
 from PyQt6.QtWidgets import QApplication, QMessageBox, QTableWidgetItem
+from dotenv import dotenv_values
+from db.database import DataBase
 from handlers.account import Account
 from handlers.forgot_password import Password
-from handlers.new_password import New_password
+from handlers.new_password import NewPassword
 from handlers.regist import Registration
-from handlers.sign_up import Sign_up
-from dotenv import dotenv_values
+from handlers.sign_up import SignUp
+
 
 config = dotenv_values(".env")
 
 db = DataBase(config)
 win = QApplication([])
 
-login = Sign_up("designs/regist.ui", db, "Войти")
+login = SignUp("designs/regist.ui", db, "Войти")
 account = Account("designs/personal_data.ui", db, "Личный кабинет")
 regist = Registration("designs/registration.ui", db, "Регистрация")
 forgot_password = Password("designs/password.ui", db, "Забыли пароль")
-new_password = New_password("designs/new_password.ui", db, "Новый пароль")
+new_password = NewPassword("designs/new_password.ui", db, "Новый пароль")
 login.show()
 win.exec()
